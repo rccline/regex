6757c211043ff45030b0a75203d2e0f06588e40b
Robert Cline <rccline@fastmail.fm>
2022-09-11 09:51:24 -0600

    Added callout-tip to qmd and added script to download url to folder

--- scripts/download2data.R
+++ scripts/download2data.R
@@ -9,4 +9,5 @@ fs::dir_create(here("data"))
 download.file(
   url = "https://raw.githubusercontent.com/cosmoduende/r-marvel-vs-dc/main/dataset_shdb/heroesInformation.csv", 
   destfile = here("data/comics.csv")
-)
+) 
+
